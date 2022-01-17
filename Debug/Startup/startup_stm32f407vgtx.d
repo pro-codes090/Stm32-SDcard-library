a535f8aec539ca98c3088eec1429675e8690088c
pratham vora (procodes090) <procodes090@gmail.com>
2022-01-18 03:58:00 +0530

    test done

--- Debug/Startup/startup_stm32f407vgtx.d
+++
@@ -1,1 +0,0 @@
-Startup/startup_stm32f407vgtx.o: ../Startup/startup_stm32f407vgtx.s
--- Debug/drivers/Src/stm32f407XX_spi_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/Src/stm32f407XX_spi_driver.o: \
- ../drivers/Src/stm32f407XX_spi_driver.c \
- C:/Users/pro/Documents/stm32drivers/stm32f4_sdcard_spi/drivers/Inc/stm32f407xx_spi_driver.h \
- C:/Users/pro/Documents/stm32drivers/stm32f4_sdcard_spi/drivers/Inc/stm32f407xx.h
-
-C:/Users/pro/Documents/stm32drivers/stm32f4_sdcard_spi/drivers/Inc/stm32f407xx_spi_driver.h:
-
-C:/Users/pro/Documents/stm32drivers/stm32f4_sdcard_spi/drivers/Inc/stm32f407xx.h:
