5ad8e173affe5e77248b863088d2c906e3844067
pratham vora (procodes090) <procodes090@gmail.com>
2022-01-29 20:43:14 +0530

    sdcard .c and .h modified added

--- drivers/Inc/stm32_SDcard.h
+++ drivers/Inc/stm32_SDcard.h
@@ -9,5 +9,10 @@
 #define INC_STM32_SDCARD_H_
 
 
+void readBlockSingle(uint32_t blockIndex ) ;
+void writeBlockSingle(uint32_t blockIndex , uint8_t data) ;
+void readBlockMultiple(uint32_t blockIndex ) ;
+void writeBlockMultiple() ;
+void stopTransfer() ;
 
 #endif /* INC_STM32_SDCARD_H_ */
---
+++ drivers/Src/stm32_SDcard.c
@@ -0,0 +1,8 @@
+/*
+ * stm32_SDcard.c
+ *
+ *  Created on: 29-Jan-2022
+ *      Author: pro
+ */
+
+
